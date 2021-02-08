95ca6e9c55c700d84d679fa4bc126158fa4ef827
Muhammed Hanifi Alma <hanifi.alma2014.mha@gmail.com>
2021-02-08 19:27:30 +0300

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Muhammed Hanifi Alma on 8.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
